40163c5b68d0912da9920fcb05801cfd4511bcc6
Enea <eneasecondary@gmail.com>
2018-12-06 14:51:11 -0800

    Fixed the thumbnails to be mobile responsive

--- my-app/src/app/card-tile/card-tile.component.ts
+++ my-app/src/app/card-tile/card-tile.component.ts
@@ -13,24 +13,18 @@ export class CardTileComponent implements OnInit {
   constructor() {
     this.new = {
       title: "NEW",
-      summary:
-        "summary1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-      image:
-        "http://www.airport-data.com/images/aircraft/thumbnails/001/288/001288330.jpg"
+      summary: "Start a new Contract",
+      image: "../assets/images/new1.jpg"
     };
     this.current = {
       title: "CURRENT",
-      summary:
-        "summary2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-      image:
-        "http://www.airport-data.com/images/aircraft/thumbnails/001/288/001288330.jpg"
+      summary: "View existing Contracts",
+      image: "../assets/images/current1.jpg"
     };
     this.archived = {
       title: "ARCHIVED",
-      summary:
-        "summary3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-      image:
-        "http://www.airport-data.com/images/aircraft/thumbnails/001/288/001288330.jpg"
+      summary: "View Archived Contracts",
+      image: "../assets/images/archived1.jpg"
     };
   }
 
