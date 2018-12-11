ec1389f0949dbfd0ea74df66a4fbbac6cc562cf4
Enea <eneasecondary@gmail.com>
2018-12-11 09:08:39 -0800

    Added project number on drop down selection

--- my-app/src/app/pages/page-hard-stops/page-hard-stops.component.ts
+++ my-app/src/app/pages/page-hard-stops/page-hard-stops.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from "@angular/core";
 export class HardStopsComponent implements OnInit {
   questions = ["YES", "NO", "I DON'T KNOW"];
   submitted = false;
+  questionAnswer = [];
 
   onSubmit() {
     this.submitted = true;
